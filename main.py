4bdd6058c329448a70025153b68385807f9f8d24
ZahirulAlim <zahirulalim@upi.edu>
2024-07-07 03:59:21 +0700

    commit kedua

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8n.pt")
-results = model(source=0,show=True, conf=0)
+model = YOLO("yolov8n-seg.pt")
+results = model(source=0, show=True, conf=0.5)
